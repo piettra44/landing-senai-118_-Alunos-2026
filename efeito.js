86cd6e8cb2de596a7caf23ddb2ca6859fa30d462
piettra44 <almeidapietra545@gmail.com>
2026-02-12 14:19:17 -0300

    commit eu

---
+++ efeito.js
@@ -0,0 +1,11 @@
+//Captura o botao e o menu
+
+const botao = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+
+//Cria o evento de clique
+
+botao.addEventListener('click', ()=>{
+    //Ativa e desativa a classe aberto (Usando: Toggle)
+    menu.classList.toggle('aberto')
+});
